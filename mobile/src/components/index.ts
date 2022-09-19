f43e22f31bcd603d7846937d4b9c209e711f8be4
Lucas da Silva Bezerra <lucasdsilvab@gmail.com>
2022-09-19 20:25:03 -0300

    NLW eSports

---
+++ mobile/src/components/index.ts
@@ -0,0 +1,11 @@
+import {Background} from './Background' 
+import {DuoCard, DuoCardProps} from './DuoCard' 
+import {DuoInfo} from './DuoInfo' 
+import {DuoMatch} from './DuoMatch' 
+import {GameCard} from './GameCard' 
+import {Loading} from './Loading' 
+import {Header} from './Header'
+
+export {
+  Background, DuoCard, DuoInfo, DuoMatch, GameCard,Loading, DuoCardProps, Header
+}
